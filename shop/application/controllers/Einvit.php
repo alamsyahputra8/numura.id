5c7d62b407be0e41844a455ac0b2b3b595923cb9
Alamsyah S Putra <aspsyahputra@gmail.com>
2021-10-04 10:25:46 +0700

    cek upload

--- shop/application/controllers/Einvit.php
+++ shop/application/controllers/Einvit.php
@@ -232,6 +232,8 @@ class Einvit extends CI_Controller {
 				$nama_file[$jb]      	= $_FILES['banner']['name'][$jb];
 				$nama_file_unik[$jb] 	= str_replace(' ','_',$nama_file[$jb]);
 
+				echo $nama_file_unik[$jb];
+
 				$extension 				= pathinfo($nama_file[$jb], PATHINFO_EXTENSION);
 				if (!empty($lokasi_file[$jb])){
 					$vfile_upload[$jb] = $dirfile . $nama_file_unik[$jb];
