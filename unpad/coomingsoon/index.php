090f2f6297127b82a0da8e5ac620bdb60c71da59
Alamsyah S Putra <aspsyahputra@gmail.com>
2022-04-11 13:46:51 +0700

    cek push theme polo 2

---
+++ unpad/coomingsoon/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />    <meta name="author" content="ARCA INDONESIA" />
+	<meta name="description" content="PT AMARINDO MANDIRI - Our website is under construction, we are working very hard to give you the best experience on our new web site.">
+    <link rel="icon" type="image/png" href="images/faviconams.png">   
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Document title -->
+    <title>PT AMARINDO MANDIRI</title>
+    <!-- Stylesheets & Fonts -->
+    <link href="css/plugins.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+    <style>
+        .fullscreen .container, .fullscreen .container-wide {
+            margin-top: 20px;
+        }
+    </style>
+</head> 
+
+<body>
+
+    <!-- Body Inner -->
+    <div class="body-inner">
+        <section class="fullscreen text-center text-light background-overlay" data-bg-video="homepages/video-background-dark/video/Typing/Typing.mp4">
+            <div class="bg-overlay" style="opacity: 1;"></div>
+            <div class="container">
+                <div data-animate="fadeInUp">
+                    <h1>PT AMARINDO MANDIRI<br>SERVICE</h1>
+                </div>
+                <div class="countdown circle countdown-light m-b-40" data-countdown="2021/05/07 00:00:00" data-animate="fadeInUp"></div>
+
+                <div data-animate-delay="600" data-animate="fadeInUp">
+
+                    <p class="lead">Our website is under construction, we are working very hard to give you the best experience on our new web site.</p>
+                    <small>
+                        <span style="padding-right: 10px;">
+                            <i class="fa fa-phone"></i> (021) 727-384-96
+                        </span>|
+                        <span style="padding-left: 10px;">
+                            <i class="fab fa-whatsapp"></i> 0819-0331-1316
+                        </span>
+                    </small>
+                </div>
+                <hr class="space">
+                <!-- <div class="row">
+                    <div class="col-6 center">
+                        <div data-animate-delay="600" data-animate="fadeInUp">
+                            <div class="widget clearfix widget-newsletter">
+                                <form class="widget-subscribe-form" novalidate action="include/subscribe-form.php" role="form" method="post">
+                                    <small>Stay ready, we`re launching soon</small>
+                                    <div class="input-group form-control-lg">
+                                        <input type="email" required name="widget-subscribe-form-email" class="form-control required email" placeholder="Enter your Email">
+                                        <span class="input-group-btn input-large">
+                                            <button type="submit" id="widget-subscribe-submit-button" class="btn btn-danger">Subscribe</button>
+                                        </span> </div>
+                                </form>
+
+                            </div>
+                        </div>
+                    </div>
+                </div> -->
+
+
+                <!-- <hr class="space"> -->
+                <div class="text-light" data-animate-delay="800" data-animate="fadeInUp">&copy; 2021 PT AMARINDO MANDIRI. All Rights Reserved.</div>
+            </div>
+
+        </section>
+    </div>
+    <!-- end: Body Inner -->
+
+    <!-- Scroll top -->
+    <a id="scrollTop"><i class="icon-chevron-up"></i><i class="icon-chevron-up"></i></a>
+    <!--Plugins-->
+    <script src="js/jquery.js"></script>
+    <script src="js/plugins.js"></script>
+
+    <!--Template functions-->
+    <script src="js/functions.js"></script>
+
+</body>
+
+</html>
